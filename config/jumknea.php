aadc60a0fdd9d3c9d076a64e92b935f7fc02f5a1
meyngim <2.66635478e+08+meyngim@users.noreply.github.com>
2026-08-14 11:49:54 +0000

    chore: configure confirmed admin role emails

--- config/jumknea.php
+++ config/jumknea.php
@@ -14,12 +14,12 @@ return [
     'admins' => [
         'super_admin' => [
             'name' => env('SUPER_ADMIN_NAME', 'JumKneaBooth Super Admin'),
-            'email' => env('SUPER_ADMIN_EMAIL'),
+            'email' => env('SUPER_ADMIN_EMAIL', 'meyngim22@gmail.com'),
             'password' => env('SUPER_ADMIN_PASSWORD'),
         ],
         'admin' => [
             'name' => env('ADMIN_NAME', 'JumKneaBooth Admin'),
-            'email' => env('ADMIN_EMAIL'),
+            'email' => env('ADMIN_EMAIL', 'solodev.me@gmail.com'),
             'password' => env('ADMIN_PASSWORD'),
         ],
     ],
